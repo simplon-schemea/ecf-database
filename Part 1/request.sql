29f8930a066bc1bbf0efe65efd49c7b3977cc539
CHEA Stephane <stephane.s.chea@renault.com>
2020-10-21 19:30:16 +0200

    add aliases on columns

--- Part 1/request.sql
+++ Part 1/request.sql
@@ -1,18 +1,18 @@
 -- PART 1: REQUEST
 
-SELECT matricule, lastname, firstname, tech.name_fr
+SELECT matricule, lastname, firstname, tech.name_fr as main_tech_fr
 FROM developers dev
          LEFT JOIN developer_technology dt ON dev.id = dt.developer_id
          INNER JOIN technologies tech ON dev.main_technology_id = tech.id
 WHERE dt.technology_id IS NULL;
 
-SELECT matricule, lastname, firstname, count(*) AS learnt_tech
+SELECT matricule, lastname, firstname, count(*) AS learnt_techs
 FROM developers dev
          INNER JOIN developer_technology dt ON dev.id = dt.developer_id
 WHERE dt.technology_id != dev.main_technology_id
 GROUP BY dev.id;
 
-SELECT matricule, lastname, firstname, tech.name_en
+SELECT matricule, lastname, firstname, tech.name_en as main_tech_en
 FROM developer_technology l
          INNER JOIN developer_technology r ON l.technology_id = r.technology_id AND l.developer_id != r.developer_id
          INNER JOIN developers dev ON dev.id = l.developer_id
--- Part 2/request.sql
+++ Part 2/request.sql
@@ -1,6 +1,6 @@
 -- PART 2: REQUEST
 
-SELECT lead.lastname, lead.firstname, count(*)
+SELECT lead.lastname, lead.firstname, count(*) as tech_lead_of
 FROM developers lead
          INNER JOIN developers dev ON lead.id = dev.tech_lead_id
 GROUP BY lead.lastname, lead.firstname;
